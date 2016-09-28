c2135edc42b5129a559481f8dc47fa18239284e5
umesh <umesh.gattem@gmail.com>
2016-09-28 21:24:34 +0530

    Added network model for iris dataset

--- tensorflow_programs/Network_module_for_iris_dataset.py
+++ tensorflow_programs/Network_module_for_iris_dataset.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import numpy as np
 import pandas as pd
 
-read_file = pd.read_csv("/home/umesh/sample.csv")
+read_file = pd.read_csv("iris_data.csv")
 my_input = []
 my_output = []
 for index, row in read_file.iterrows():
@@ -12,18 +12,12 @@ for index, row in read_file.iterrows():
     my_output.append(output_data)
 train_data_input = np.array(my_input)
 train_data_output = np.array(my_output)
-# print(train_data_input)
-# print(train_data_output.shape)
-# exit()
 
 input_list = []
 output_list = []
 for (x, y) in zip(train_data_input, train_data_output):
     x = np.array([x])
     y = np.array([y])
-    # print(x.shape)
-    # print(y.shape)
-    # exit()
     input_list.append(x)
     output_list.append(y)
 
@@ -32,11 +26,11 @@ no_of_inputs = len(train_data_input)
 input_data = tf.placeholder("float")
 output_data = tf.placeholder("float")
 
-weights = tf.Variable(tf.truncated_normal((4, 3), dtype=tf.float32))
-bias = tf.Variable(tf.truncated_normal((1, 3), dtype=tf.float32))
+weights = tf.Variable(tf.random_normal((4, 3), dtype=tf.float32))
+bias = tf.Variable(tf.random_normal((1, 3), dtype=tf.float32))
 
 learning_rate = 0.01
-epochs = 10000
+epochs = 1000
 
 activation = tf.add(tf.matmul(input_data, weights), bias)
 
@@ -51,18 +45,25 @@ with tf.Session() as sess:
     for each_epoch in range(epochs):
         for (x, y) in zip(input_list, output_list):
             sess.run(train_step, feed_dict={input_data: x, output_data: y})
-        if ((each_epoch + 1) % 50) == 0:
+        if ((each_epoch + 1) % 1) == 0:
             training_cost = sess.run(cost, feed_dict={input_data: train_data_input, output_data: train_data_output})
-            print("Epoch", each_epoch+1, ":", training_cost)
+            print("Epoch", each_epoch+1, ":", "cost is :", training_cost)
     print("Training completed!")
     print("Training cost=", training_cost, "W=", sess.run(weights), "b=", sess.run(bias), '\n')
 
     test_output = []
     for x in input_list:
         output = sess.run(activation, feed_dict={input_data: x})
-        # max_index = output.index(max(output))
-        # output = [0] * 3
-        # output[max_index] = 1
-        test_output.append(output)
-    print(test_output)
+        index = np.argmax(output)
+        result = np.zeros(output.shape, dtype=float)
+        result[0, index] = 1
+        test_output.append(result)
+    count = 0
+    for x, y in zip(test_output, output_list):
+        if (x == y).all():
+            count += 1
+    accuracy = count/len(train_data_input) * 100
+    print("Accuracy: ", accuracy)
+
+
 
